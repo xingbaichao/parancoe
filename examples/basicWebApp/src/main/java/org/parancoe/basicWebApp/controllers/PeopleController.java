aaa4a23a232a592e3c775dd35c3b5f971a202c66
benfante <benfante@localhost>
2007-01-17 18:12:33 +0000

    Added populate action

--- examples/basicWebApp/src/main/java/org/parancoe/basicWebApp/controllers/PeopleController.java
+++ examples/basicWebApp/src/main/java/org/parancoe/basicWebApp/controllers/PeopleController.java
@@ -1,5 +1,6 @@
 package org.parancoe.basicWebApp.controllers;
 
+import java.text.ParseException;
 import org.parancoe.web.BaseMultiActionController;
 import org.parancoe.basicWebApp.Daos;
 import org.parancoe.basicWebApp.Blos;
@@ -20,6 +21,25 @@ public abstract class PeopleController extends BaseMultiActionController {
         return new ModelAndView("people/list", params);
     }
 
+    public ModelAndView populate(HttpServletRequest req, HttpServletResponse res){
+        Map params = new HashMap();
+        try {
+            if (blo() != null) {
+                if (blo().person != null) {
+                    blo().person.populateArchive();
+                } else {
+                    return genericError("blo().person è null");
+                }
+            } else {
+                return genericError("blo() è null");
+            }
+        } catch (ParseException ex) {
+            return genericError(ex);
+        }
+        params.put("people", dao().person.findAll());
+        return new ModelAndView("people/list", params);
+    }
+    
     public Logger getLogger() {
         return logger;
     }
