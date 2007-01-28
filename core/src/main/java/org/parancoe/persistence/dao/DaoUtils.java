d2ed13976a1b3bef32415073f60156f8bffbe2cb
benfante <benfante@localhost>
2007-01-28 23:36:51 +0000

    Added DaoProvider and facilities for populating classes with DAOs.

---
+++ core/src/main/java/org/parancoe/persistence/dao/DaoUtils.java
@@ -0,0 +1,85 @@
+// Copyright 2006 The Parancoe Team
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package org.parancoe.persistence.dao;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.parancoe.persistence.dao.generic.Dao;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanIsAbstractException;
+import org.springframework.beans.factory.ListableBeanFactory;
+
+/**
+ * Utils for the DAO tools.
+ *
+ * @author <a href="mailto:lucio.benfante@jugpadova.it">Lucio Benfante</a>
+ * @version $Revision$
+ */
+public class DaoUtils {
+    
+    /** Creates a new instance of DaoUtils */
+    private DaoUtils() {
+    }
+    
+    /**
+     * Return a map  of DAOs from a bean container.
+     *
+     * @param beanFactory The bean container.
+     * @return A map of daos. The key is the id of the bean in the container.
+     */
+    public static Map<String, Object> getDaos(ListableBeanFactory beanFactory) {
+        Map<String, Object> result = new HashMap<String, Object>();
+        String[] beanNames = beanFactory.getBeanDefinitionNames();
+        for (int i=0; i < beanNames.length; i++) {
+            try {
+                Object bean = beanFactory.getBean(beanNames[i]);
+                if (DaoUtils.isDao(bean)) {
+                    result.put(beanNames[i], bean);
+                }
+            } catch (BeanIsAbstractException ex) {
+                // ignore it
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Check if an object is a DAO.
+     *
+     * @return true if the object is recognized as a DAO.
+     */
+    public static boolean isDao(Object o) {
+        Class[] objInterfaces = o.getClass().getInterfaces();
+        for (int i = 0; i < objInterfaces.length; i++) {
+            if (objInterfaces[i].getAnnotation(Dao.class) != null) {
+                return true;
+            }            
+        }
+        return false;
+    }
+
+    public static boolean isDaoFor(Object o, Class daoEntityType) {
+        Class[] objInterfaces = o.getClass().getInterfaces();
+        for (int i = 0; i < objInterfaces.length; i++) {
+            Dao daoAnnotation = (Dao) objInterfaces[i].getAnnotation(Dao.class);
+            if (daoAnnotation != null) {
+                if (daoAnnotation.entity().getName().equals(daoEntityType.getName())) {
+                    return true;
+                }
+            }            
+        }
+        return false;
+    }
+}
---
+++ core/src/test/java/org/parancoe/persistence/dao/DaoProviderTest.java
@@ -0,0 +1,101 @@
+// Copyright 2006 The Parancoe Team
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package org.parancoe.persistence.dao;
+
+import junit.framework.*;
+import java.util.Map;
+import org.parancoe.persistence.po.hibernate.EntityTC;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.access.BeanFactoryLocator;
+import org.springframework.beans.factory.access.BeanFactoryReference;
+import org.springframework.beans.factory.access.SingletonBeanFactoryLocator;
+
+/**
+ * Tests for the DaoProvider classes.
+ *
+ * @author <a href="mailto:lucio.benfante@jugpadova.it">Lucio Benfante</a>
+ * @version $Revision$
+ */
+public class DaoProviderTest extends TestCase {
+    private BeanFactory beanFactory;
+    
+    public DaoProviderTest(String testName) {
+        super(testName);
+        BeanFactoryLocator bfl = SingletonBeanFactoryLocator.getInstance("beanRefFactory_test.xml");
+        BeanFactoryReference bf = bfl.useBeanFactory("org.parancoe.persistence");
+        this.beanFactory = bf.getFactory();
+        Map daoMap = (Map) this.beanFactory.getBean("daoMap");
+        Map daos = DaoUtils.getDaos((ListableBeanFactory) this.beanFactory);
+        daoMap.putAll(daos);
+    }
+    
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    /**
+     * Test the retrieval of the DAO map.
+     */
+    public void testGetDaoMap() {
+        Map daoMap = (Map) this.beanFactory.getBean("daoMap");
+        assertNotNull(daoMap);
+        assertTrue("Test DAO map shouldn't be empty", !daoMap.isEmpty());
+    }
+
+    /**
+     * Test the retrieval of a DAO from the DAO map.
+     */
+    public void testGetDaoFromMap() {
+        Map daoMap = (Map) this.beanFactory.getBean("daoMap");
+        assertNotNull(daoMap);
+        Object dao = daoMap.get("entityTCDao");
+        assertNotNull(dao);
+        assertTrue(DaoUtils.isDao(dao));
+    }
+
+    /**
+     * Test the retrieval of the DAO map from the generic dao provider.
+     */
+    public void testGetDaoMapFromProvider() {
+        DaoProvider baseDaoProvider = (DaoProvider) this.beanFactory.getBean("genericDaoProvider");
+        assertNotNull(baseDaoProvider);
+        Map daoMap = baseDaoProvider.getDaoMap();
+        assertNotNull(daoMap);
+        assertTrue("Test DAO map shouldn't be empty", !daoMap.isEmpty());
+    }
+
+    /**
+     * Test the retrieval of a DAO from the generic dao provider.
+     */
+    public void testGetDaoFromProvider() {
+        DaoProvider baseDaoProvider = (DaoProvider) this.beanFactory.getBean("genericDaoProvider");
+        assertNotNull(baseDaoProvider);
+        Object dao = baseDaoProvider.getDao("entityTCDao");
+        assertNotNull(dao);
+        assertTrue(DaoUtils.isDao(dao));
+    }
+
+    /**
+     * Test the retrieval of a DAO for an entity from the generic dao provider.
+     */
+    public void testGetDaoByEntityFromProvider() {
+        DaoProvider baseDaoProvider = (DaoProvider) this.beanFactory.getBean("genericDaoProvider");
+        assertNotNull(baseDaoProvider);
+        Object dao = baseDaoProvider.getDao(EntityTC.class);
+        assertNotNull(dao);
+        assertTrue(DaoUtils.isDao(dao));
+    }
+    
+}
