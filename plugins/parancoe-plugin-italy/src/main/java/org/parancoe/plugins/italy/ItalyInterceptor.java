26d4007a66045b02adeb55b86a6db4d606d46c27
paolodona <paolodona@localhost>
2007-06-02 20:52:46 +0000

    Sistemata primissima versione del plugin Italy, ora contiene anche un custom interceptor e un context listener.

---
+++ plugins/parancoe-plugin-italy/src/main/java/org/parancoe/plugins/italy/ItalyInterceptor.java
@@ -0,0 +1,24 @@
+package org.parancoe.plugins.italy;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.apache.log4j.Logger;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author paolo.dona@seesaw.it
+ */
+public class ItalyInterceptor extends HandlerInterceptorAdapter {
+  public static final Logger logger = Logger.getLogger(ItalyInterceptor.class);
+
+  public ItalyInterceptor() {
+    logger.info("ItalyInterceptor set up");
+  }
+
+  public boolean preHandle(HttpServletRequest req, HttpServletResponse res,
+                           Object object) throws Exception {
+    logger.info("ItalyInterceptor preHandle");
+    return true;
+  }
+}
