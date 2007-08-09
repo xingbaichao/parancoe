7c1b989b7c1176385bf16a1a6f8a892495db07b1
lucio.benfante <lucio.benfante@localhost>
2007-08-09 09:28:14 +0000

    Changed DB model

--- plugins/parancoe-plugin-security/src/main/java/org/parancoe/plugins/security/User.java
+++ plugins/parancoe-plugin-security/src/main/java/org/parancoe/plugins/security/User.java
@@ -1,11 +1,11 @@
 // Copyright 2006-2007 The Parancoe Team
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 // http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,21 +14,21 @@
 package org.parancoe.plugins.security;
 
 import java.util.List;
-
 import javax.persistence.CascadeType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
-
 import org.parancoe.persistence.po.hibernate.EntityBase;
 
 
 /**
  * A PO for UserProfile table.
- * 
+ *
  * @author <a href="mailto:enrico.giurin@gmail.com">Enrico Giurin</a>
  * @author <a href="mailto:michele.franzin@seesaw.it">Michele Franzin</a>
  * @version $Revision$
  */
-@javax.persistence.Entity()
+@javax.persistence.Entity
 public class User extends EntityBase {
 
     private static final long serialVersionUID = 832363948575562242L;
@@ -38,39 +38,40 @@ public class User extends EntityBase {
     private String password = null;
 
     private boolean enabled = true;
-    
-    private List<Authorities> authorities;
+
+    private List<UserAuthority> userAuthority;
 
     public boolean isEnabled() {
-	return enabled;
+        return enabled;
     }
 
     public String getPassword() {
-	return password;
+        return password;
     }
 
     public void setPassword(String password) {
-	this.password = password;
+        this.password = password;
     }
 
     public String getUsername() {
-	return username;
+        return username;
     }
 
     public void setUsername(String username) {
-	this.username = username;
+        this.username = username;
     }
 
     public void setEnabled(boolean enabled) {
-	this.enabled = enabled;
+        this.enabled = enabled;
     }
-    @OneToMany(cascade={CascadeType.ALL})
-	public List<Authorities> getAuthorities() {
-		return authorities;
-	}
 
-	public void setAuthorities(List<Authorities> authorities) {
-		this.authorities = authorities;
-	}
+    @OneToMany(mappedBy="user")
+    public List<UserAuthority> getUserAuthority() {
+        return userAuthority;
+    }
 
+    public void setUserAuthority(List<UserAuthority> userAuthority) {
+        this.userAuthority = userAuthority;
+    }
+    
 }
