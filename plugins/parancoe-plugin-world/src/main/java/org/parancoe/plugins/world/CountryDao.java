becc2148f9e07cb8984e337e09bea195556558b8
lucio.benfante <lucio.benfante@localhost>
2007-08-15 22:53:30 +0000

    Added some find methods

--- plugins/parancoe-plugin-world/src/main/java/org/parancoe/plugins/world/CountryDao.java
+++ plugins/parancoe-plugin-world/src/main/java/org/parancoe/plugins/world/CountryDao.java
@@ -8,6 +8,9 @@ import org.parancoe.persistence.dao.generic.GenericDao;
 @Dao(entity = Country.class)
 public interface CountryDao extends GenericDao<Country, String> {
   List<Country> findByPartialLocalName(String partialLocalName);
+  List<Country> findByPartialLocalNameAndContinent(String partialLocalName, String continent);
+  List<Country> findByPartialEnglishName(String partialEnglishName);
+  List<Country> findByPartialEnglishNameAndContinent(String partialLocalName, String continent);
   /**
    * Search Country by isoCode
    * @author Enrico Giurin
