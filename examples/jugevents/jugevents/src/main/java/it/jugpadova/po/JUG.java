fb73ed64de94d31e92210951f26dfd8ba1e9098e
lucio.benfante <lucio.benfante@localhost>
2007-08-29 10:58:32 +0000

    Fixed search query. Simplified JUG entity.

--- examples/jugevents/jugevents/src/main/java/it/jugpadova/po/JUG.java
+++ examples/jugevents/jugevents/src/main/java/it/jugpadova/po/JUG.java
@@ -1,13 +1,11 @@
 /**
- * 
+ *
  */
 package it.jugpadova.po;
 
 import java.net.URI;
-
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-
 import org.parancoe.persistence.po.hibernate.EntityBase;
 import org.parancoe.plugins.world.Country;
 import org.springmodules.validation.bean.conf.loader.annotation.handler.CascadeValidation;
@@ -20,73 +18,30 @@ import org.springmodules.validation.bean.conf.loader.annotation.handler.NotBlank
  */
 @Entity
 public class JUG extends EntityBase {
-	/**
-	 * JUG name, i.e. Padova
-	 */
-	@NotBlank
-	private String name;
-	/**
-	 * web site JUG, i.e. www.jugpadova.it
-	 */
-	private URI website;
-	/**
-	 * address of meeting.
-	 */
-	private String address;
-	
-	@CascadeValidation
-	private Country country;
-	
-	private String infoMail;
-	/**
-	 * email of jug leader.
-	 */
-	private String leaderMail;
-	private String info;
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	@ManyToOne
-	public Country getCountry() {
-		return country;
-	}
-	public void setCountry(Country country) {
-		this.country = country;
-	}
-	public String getInfo() {
-		return info;
-	}
-	public void setInfo(String info) {
-		this.info = info;
-	}
-	public String getInfoMail() {
-		return infoMail;
-	}
-	public void setInfoMail(String infoMail) {
-		this.infoMail = infoMail;
-	}
-	public String getLeaderMail() {
-		return leaderMail;
-	}
-	public void setLeaderMail(String leaderMail) {
-		this.leaderMail = leaderMail;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public URI getWebsite() {
-		return website;
-	}
-	public void setWebsite(URI website) {
-		this.website = website;
-	}
-	
-	
 
+    /**
+     * JUG name
+     */
+    @NotBlank
+    private String name;
+
+    @CascadeValidation
+    private Country country;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    @ManyToOne
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
 }
