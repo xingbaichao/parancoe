ded6eac85266d6f1122de46efb86e8c2987b42e2
lucio.benfante <lucio.benfante@localhost>
2007-09-01 05:18:54 +0000

    Fixed AJAX loading message

--- examples/jugevents/jugevents/src/main/webapp/javascripts/parancoe.js
+++ examples/jugevents/jugevents/src/main/webapp/javascripts/parancoe.js
@@ -1,7 +1,7 @@
 function Parancoe() {
     function Util() {
-        this.initDWR = function () {
-            dwr.util.useLoadingMessage();
+        this.initDWR = function () {            
+            useLoadingMessage();
             dwr.engine.reverseAjax = true;
         };
     }
@@ -10,3 +10,35 @@ function Parancoe() {
 }
 
 var parancoe = new Parancoe();
+
+function useLoadingMessage(message) {
+  var loadingMessage;
+  if (message) loadingMessage = message;
+  else loadingMessage = "Loading...";
+
+  dwr.engine.setPreHook(function() {
+    var messageZone = $('messageZone');
+    if (!messageZone) {
+      messageZone = document.createElement('div');
+      messageZone.setAttribute('id', 'messageZone');
+      messageZone.style.position = "fixed";
+      messageZone.style.top = "0px";
+      messageZone.style.right = "0px";
+      messageZone.style.background = "red";
+      messageZone.style.color = "white";
+      messageZone.style.fontFamily = "Arial,Helvetica,sans-serif";
+      messageZone.style.padding = "4px";
+      document.body.appendChild(messageZone);
+      var text = document.createTextNode(loadingMessage);
+      messageZone.appendChild(text);
+    }
+    else {
+      messageZone.innerHTML = loadingMessage;
+      messageZone.style.visibility = 'visible';
+    }
+  });
+
+  dwr.engine.setPostHook(function() {
+    $('messageZone').style.visibility = 'hidden';
+  });
+}
