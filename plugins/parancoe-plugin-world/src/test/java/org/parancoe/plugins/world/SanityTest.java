a1a5b142b7c7276e75a48c0a6d9cb9c157ab8e08
lucio.benfante <lucio.benfante@localhost>
2007-09-05 07:34:00 +0000

    Added Senegal and Indonesia. Added and fixed some methods to the DAOs.

--- plugins/parancoe-plugin-world/src/test/java/org/parancoe/plugins/world/SanityTest.java
+++ plugins/parancoe-plugin-world/src/test/java/org/parancoe/plugins/world/SanityTest.java
@@ -9,9 +9,8 @@ public class SanityTest extends PluginTest {
     private Plugin plugin;
 
     public SanityTest() {
-        super();
     }
-    
+
     @Override
     public void setUp() throws Exception {
         super.setUp();
@@ -25,42 +24,55 @@ public class SanityTest extends PluginTest {
     }
 
     public void testContinentAssigned() {
-        CountryDao countryDao = (CountryDao) this.ctx.getBean("countryDao");
+        CountryDao countryDao =
+                (CountryDao) this.ctx.getBean("countryDao");
         List<Country> countries = countryDao.findAll();
         assertNotEmpty(countries);
         for (Country c : countries) {
             assertNotNull(c.getContinent());
         }
     }
-    
+
     public void testContinentSize() {
-        ContinentDao continentDao = (ContinentDao) this.ctx.getBean("continentDao");
+        ContinentDao continentDao =
+                (ContinentDao) this.ctx.getBean("continentDao");
         List<Continent> continents = continentDao.findAll();
         assertSize(7, continents);
     }
-    
-    
+
+
     public void testFindAllOrderedByEnglishNameAsc() {
-    	 CountryDao countryDao = (CountryDao) this.ctx.getBean("countryDao");
-         List<Country> countries = countryDao.findAllOrderedByEnglishNameAsc();
-         assertEquals(countries.get(0).getEnglishName(), "Albania");
+        CountryDao countryDao =
+                (CountryDao) this.ctx.getBean("countryDao");
+        List<Country> countries = countryDao.findByOrderByEnglishName();
+        assertEquals(countries.get(0).getEnglishName(), "Albania");
     }
-    
-    
+
+
     public void testCountryFindByIsocode() {
-    	CountryDao countryDao = (CountryDao) this.ctx.getBean("countryDao");
-    	 List<Country> countries = countryDao.findByIsoCode("IQ");
-    	
-        assertEquals(countries.get(0).getEnglishName(), "Iraq");
+        CountryDao countryDao =
+                (CountryDao) this.ctx.getBean("countryDao");
+        Country country = countryDao.findByIsoCode("IQ");
+        assertEquals(country.getEnglishName(), "Iraq");
     }
+
     public void testCountrySize() {
-        CountryDao countryDao = (CountryDao) this.ctx.getBean("countryDao");
+        CountryDao countryDao =
+                (CountryDao) this.ctx.getBean("countryDao");
         List<Country> countries = countryDao.findAll();
-        assertSize(87, countries);
+        assertSize(89, countries);
+    }
+
+    public void testContinentFindByName() {
+        ContinentDao continentDao =
+                (ContinentDao) this.ctx.getBean("continentDao");
+        Continent europe = continentDao.findByName("Europe");
+        assertNotNull(europe);
+        assertEquals("Europe", europe.getName());
     }
 
     @Override
     public Class[] getFixtureClasses() {
-        return new Class[] {Continent.class, Country.class};
+        return new Class[]{Continent.class, Country.class};
     }
 }
