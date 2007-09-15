3beedf12e6c01eeaa2ff158e43b0a18267bc87fd
andrea.nasato <andrea.nasato@localhost>
2007-09-15 14:17:56 +0000

    + license + uncamelize method

--- parancoe-core/src/main/java/org/parancoe/util/Utils.java
+++ parancoe-core/src/main/java/org/parancoe/util/Utils.java
@@ -1,3 +1,16 @@
+// Copyright 2006-2007 The Parancoe Team
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package org.parancoe.util;
 
 import java.io.IOException;
@@ -9,6 +22,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.log4j.Logger;
@@ -18,6 +33,7 @@ import org.springframework.core.io.Resource;
 /**
  * @author Paolo Dona paolo.dona@seesaw.it
  * @author Michele Franzin paolo.dona@seesaw.it
+ * @author Andrea Nasato <mailto:andrea.nasato@jugpadova.it/>
  */
 public class Utils {
 
@@ -165,4 +181,61 @@ public class Utils {
     public static boolean hasUTF8preamble(String s) {
         return s.startsWith(UTF8_UNICODE_PREAMBLE);
     }
+    
+    /**
+     * Return an array containing all the substrings of
+     * <code>camelString</code>, according to this rule: divide <code>camelString</code>
+     * into tokens with every token starting from a capital letter to another.
+     *
+     * Some examples:
+     * <ol>
+     *  <li>'MyCamelString' gives: {my,camel,string}</li>
+     *  <li>'myFantasticCamelString' gives: {my,fantastic,camel,string}</li>
+     *  <li>'MYCAMELSTRING' gives: {m,y,c,a,m,e,l,s,t,r,i,n,g}</li>
+     * </ol>
+     *
+     */
+    public static String[] uncamelize(String camelString) {
+
+        if (camelString == null || camelString.trim().equals("")) {
+            throw new IllegalArgumentException("camelString cannot be null or empty");
+        }
+        List<Integer> idxList = new ArrayList<Integer>();
+        List<String> strList = new ArrayList<String>();
+
+        //\p{Lu} is the unicode pattern for capital letters
+        Pattern p = Pattern.compile("\\p{Lu}");
+        Matcher m = p.matcher(camelString);
+
+        //find all occurences of an uppercase letter and put their position on a list
+        while (m.find()) {
+            idxList.add(m.start());
+        }
+
+        // no upper case found: we return the entire word
+        if (idxList.size() == 0) {
+            return new String[]{camelString};
+        }
+
+        Integer[] idx = idxList.toArray(new Integer[idxList.size() - 1]);
+
+        for (int i = 0; i < idx.length; i++) {
+
+            //if the first character of camelString is lower case,
+            //substring from 0 to index[1]
+            if (i == 0 && idx[i] > 0) {
+                strList.add(camelString.substring(0, idx[i]).toLowerCase());
+            }
+
+            //the last part of the word hasn't got an end guard
+            if (i == idx.length - 1) {
+                strList.add(camelString.substring(idx[i], camelString.length()).toLowerCase());
+            } else {
+                strList.add(camelString.substring(idx[i], idx[i + 1]).toLowerCase());
+            }
+        }
+
+        return strList.toArray(new String[strList.size() - 1]);
+    }
+    
 }
