359fd1648329c913b8e3db79e6ea36af7aba8187
lucio.benfante <lucio.benfante@localhost>
2007-09-18 09:33:00 +0000

    Fixed some IE layouts

--- examples/jugevents/jugevents/src/main/webapp/javascripts/parancoe.js
+++ examples/jugevents/jugevents/src/main/webapp/javascripts/parancoe.js
@@ -21,13 +21,7 @@ function useLoadingMessage(message) {
     if (!messageZone) {
       messageZone = document.createElement('div');
       messageZone.setAttribute('id', 'messageZone');
-      messageZone.style.position = "fixed";
-      messageZone.style.top = "0px";
-      messageZone.style.right = "0px";
-      messageZone.style.background = "red";
-      messageZone.style.color = "white";
-      messageZone.style.fontFamily = "Arial,Helvetica,sans-serif";
-      messageZone.style.padding = "4px";
+                   
       document.body.appendChild(messageZone);
       var text = document.createTextNode(loadingMessage);
       messageZone.appendChild(text);
