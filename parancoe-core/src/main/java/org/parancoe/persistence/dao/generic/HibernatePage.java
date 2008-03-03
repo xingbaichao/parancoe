ef0c03052c2fbbf6b51f88319ab3c4246191fe0b
jacopo.murador <jacopo.murador@localhost>
2008-03-03 08:51:46 +0000

    Pagination support

---
+++ parancoe-core/src/main/java/org/parancoe/persistence/dao/generic/HibernatePage.java
@@ -0,0 +1,47 @@
+package org.parancoe.persistence.dao.generic;
+
+import java.util.List;
+
+/**
+ * @author Jacopo Murador <jacopo.murador at seesaw.it>
+ */
+public class HibernatePage<T> {
+
+    private List results;
+    private int pageSize;
+    private int page;
+    private int rowCount;
+
+    public HibernatePage(List<T> list, int page, int pageSize, int rowCount) {
+        this.page = page;
+        this.pageSize = pageSize;
+        this.rowCount = rowCount;
+        results = list;
+    }
+    
+    public int getPage() {
+        return page;
+    }
+
+    public boolean isNextPage() {
+        return page < getLastPage();
+    }
+
+    public boolean isPreviousPage() {
+        return page > 1;
+    }
+
+    public List<T> getList() {
+        return results;
+    }
+    
+    public int getLastPage() {
+        int lastPage = rowCount/pageSize;
+        if (rowCount%pageSize > 0) lastPage++;
+        return lastPage;
+    }
+
+    public int getRowCount() {
+        return rowCount;
+    }
+}
