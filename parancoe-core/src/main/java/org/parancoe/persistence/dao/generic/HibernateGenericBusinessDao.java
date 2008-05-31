f3908d8b5aa2e0a6a23726a0d9d5a267d44ee3a2
paolo.dona <paolo.dona@localhost>
2008-05-31 10:46:40 +0000

    fixed compilation issues

--- parancoe-core/src/main/java/org/parancoe/persistence/dao/generic/HibernateGenericBusinessDao.java
+++ parancoe-core/src/main/java/org/parancoe/persistence/dao/generic/HibernateGenericBusinessDao.java
@@ -1,72 +1,93 @@
-// Copyright 2006-2007 The Parancoe Team
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-// http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ *  Copyright 2008 jacopo.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
 package org.parancoe.persistence.dao.generic;
 
 import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
 import java.util.List;
-import org.apache.commons.lang.ArrayUtils;
+import javax.persistence.MappedSuperclass;
 import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.DetachedCriteria;
-
+import org.hibernate.criterion.Example;
 import org.hibernate.criterion.Projections;
 import org.hibernate.metadata.ClassMetadata;
-import org.hibernate.metadata.CollectionMetadata;
 import org.hibernate.type.Type;
+import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.dao.support.DataAccessUtils;
 
 /**
- * Hibernate implementation of the generic DAO.
  *
- * Derived from http://www-128.ibm.com/developerworks/java/library/j-genericdao.html
- *
- * @author <a href="mailto:lucio.benfante@jugpadova.it">Lucio Benfante</a>
- * @version $Revision$
+ * @author jacopo
  */
-public class HibernateGenericDao <T, PK extends Serializable>
-        extends HibernateDaoSupport
-        implements GenericDao<T, PK> {
-    private Class type;
-            
+public class HibernateGenericBusinessDao<T, PK extends Serializable> extends HibernateDaoSupport implements GenericDaoBase<T, PK>
+         {
+
+
+    private Class<T> persistentClass;
+
+    public HibernateGenericBusinessDao() {
+        this.persistentClass = (Class<T>) ((ParameterizedType) getClass()
+                                .getGenericSuperclass()).getActualTypeArguments()[0];
+    }
+
+
+    public Class<T> getPersistentClass() {
+        return persistentClass;
+    }
+
+    @Autowired
+    public void initDao(SessionFactory sessionFactory) {
+        setSessionFactory(sessionFactory);
+    }
+
     @SuppressWarnings("unchecked")
     public void create(T o) {
-        getHibernateTemplate().persist(o);
+       getHibernateTemplate().persist(o);
     }
-    
+
     @SuppressWarnings("unchecked")
     public void store(T o) {
-        getHibernateTemplate().merge(o);
+       getHibernateTemplate().merge(o);
     }
-    
+
     @SuppressWarnings("unchecked")
     public T read(PK id) {
-        return (T) getHibernateTemplate().load(getType(), id);
+        return (T) getHibernateTemplate().load(persistentClass, id);
     }
-    
+
     public void delete(T o) {
         getHibernateTemplate().delete(o);
     }
 
     @SuppressWarnings("unchecked")
     public List<T> findAll() {
-        return getHibernateTemplate().find("from "+getType().getName()+" x");
+        return getHibernateTemplate().find("from "+persistentClass.getName()+" x");
     }
 
     @SuppressWarnings("unchecked")
     public List<T> searchByCriteria(Criterion... criterion) {
-        Criteria crit = getSession().createCriteria(getType());
+        Criteria crit = getSession().createCriteria(persistentClass);
         for (Criterion c: criterion) {
             crit.add(c);
         }
@@ -82,26 +103,26 @@ public class HibernateGenericDao <T, PK extends Serializable>
     public List<T> searchByCriteria(DetachedCriteria criteria, int firstResult, int maxResults) {
         return getHibernateTemplate().
                 findByCriteria(criteria, firstResult, maxResults);
-    }    
-    
+    }
+
     @SuppressWarnings("unchecked")
     public HibernatePage<T> searchPaginatedByCriteria(int page, int pageSize, Criterion... criterion) {
-        Criteria crit = getSession().createCriteria(getType());
-        Criteria count = getSession().createCriteria(getType());
+        Criteria crit = getSession().createCriteria(persistentClass);
+        Criteria count = getSession().createCriteria(persistentClass);
         for (Criterion c: criterion) {
             crit.add(c);
             count.add(c);
         }
-        
+
         // row count
         count.setProjection(Projections.rowCount());
         int rowCount = ((Integer)count.list().get(0)).intValue();
-        
+
         crit.setFirstResult((page-1)*pageSize);
         crit.setMaxResults(pageSize);
         return new HibernatePage<T>(crit.list(), page, pageSize, rowCount);
     }
-    
+
     @SuppressWarnings("unchecked")
     public HibernatePage<T> searchPaginatedByCriteria(int page, int pageSize, DetachedCriteria criteria) {
         // Row count
@@ -110,24 +131,23 @@ public class HibernateGenericDao <T, PK extends Serializable>
                 findByCriteria(criteria).get(0)).intValue();
         criteria.setProjection(null);
         criteria.setResultTransformer(Criteria.ROOT_ENTITY);
-        
+
         List<T> list = getHibernateTemplate().
                 findByCriteria(criteria, (page-1)*pageSize, pageSize);
-        
+
         return new HibernatePage<T>(list, page, pageSize,rowCount);
     }
-    
+
     public int deleteAll() {
         List<T> rows = findAll();
-        
+
         getHibernateTemplate().deleteAll(rows);
-        
+
         return rows.size();
     }
-    
 
     public long count() {
-        return DataAccessUtils.intResult(getHibernateTemplate().find("select count(*) from " + getType().getSimpleName()));
+        return DataAccessUtils.intResult(getHibernateTemplate().find("select count(*) from " + getPersistentClass().getSimpleName()));
     }
 
     public long countByCriteria(DetachedCriteria criteria){
@@ -135,11 +155,4 @@ public class HibernateGenericDao <T, PK extends Serializable>
        return DataAccessUtils.intResult(getHibernateTemplate().findByCriteria(criteria));
     }
 
-    public Class getType() {
-        return type;
-    }
-    
-    public void setType(Class type) {
-        this.type = type;
-    }
-}
+  }
