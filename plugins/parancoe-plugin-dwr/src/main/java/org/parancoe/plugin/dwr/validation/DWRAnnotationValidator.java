8a1b681fe9dc7e792635e24aa8a8b884670039b7
lucio.benfante <lucio.benfante@localhost>
2008-06-26 14:34:26 +0000

    Extracted DWR plugin. Fixed PeopleEdit controller of the basicWebApp in the case of a new people.

--- plugins/parancoe-plugin-dwr/src/main/java/org/parancoe/plugin/dwr/validation/DWRAnnotationValidator.java
+++ plugins/parancoe-plugin-dwr/src/main/java/org/parancoe/plugin/dwr/validation/DWRAnnotationValidator.java
@@ -1,4 +1,4 @@
-// Copyright 2006-2007 The Parancoe Team
+// Copyright 2008 The Parancoe Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package org.parancoe.web.validation;
+package org.parancoe.plugin.dwr.validation;
 
 import java.text.SimpleDateFormat;
 
