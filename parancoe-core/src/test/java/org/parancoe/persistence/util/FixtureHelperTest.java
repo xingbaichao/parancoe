6a297f5d070d08d137b9240def14dcfcf7057b7e
lucio.benfante <lucio.benfante@localhost>
2008-07-01 16:19:26 +0000

    Added a test to verify object identity in fixture loading

--- parancoe-core/src/test/java/org/parancoe/persistence/util/FixtureHelperTest.java
+++ parancoe-core/src/test/java/org/parancoe/persistence/util/FixtureHelperTest.java
@@ -18,6 +18,8 @@ import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.parancoe.persistence.po.hibernate.AuthorTC;
+import org.parancoe.persistence.po.hibernate.BookTC;
 import org.parancoe.test.DBTest;
 import org.parancoe.test.EnhancedTestCase;
 import org.parancoe.util.FixtureHelper;
@@ -29,87 +31,116 @@ import org.springframework.util.CollectionUtils;
  */
 public class FixtureHelperTest extends DBTest {
 
-	private DemoBean[] expected;
+    private DemoBean[] expected;
 
-	@Override
-	public void onSetUpBeforeTransaction() throws Exception {
-		super.onSetUpBeforeTransaction();
-		expected = new DemoBean[5];
-		expected[0] = new DemoBean("first one", new Long(356));
-		expected[1] = new DemoBean("Demo 1", new Long(6789));
-		expected[2] = new DemoBean("àèéìòù", new Long(-980000));
-		expected[3] = new DemoBean("Demo3", new Long(98000));
-		expected[4] = new DemoBean("Demo#2", new Long(-9800));
-	}
+    @Override
+    public void onSetUpBeforeTransaction() throws Exception {
+        super.onSetUpBeforeTransaction();
+        expected = new DemoBean[5];
+        expected[0] = new DemoBean("first one", new Long(356));
+        expected[1] = new DemoBean("Demo 1", new Long(6789));
+        expected[2] = new DemoBean("àèéìòù", new Long(-980000));
+        expected[3] = new DemoBean("Demo3", new Long(98000));
+        expected[4] = new DemoBean("Demo#2", new Long(-9800));
+    }
 
-	@SuppressWarnings("unchecked")
-	public void testShouldNotFailWrongFixtureDir() throws Exception {
-		Set<Class> models = new LinkedHashSet<Class>(CollectionUtils
-				.arrayToList(new Class[] { DemoBean.class }));
-		Map<Class, Object[]> result = FixtureHelper.loadFixturesFromResource(
-				"nonExistant/", models);
-		assertTrue("Le fixture non sono vuote", result.isEmpty());
-	}
+    @SuppressWarnings("unchecked")
+    public void testShouldNotFailWrongFixtureDir() throws Exception {
+        Set<Class> models = new LinkedHashSet<Class>(
+                CollectionUtils.arrayToList(new Class[]{DemoBean.class}));
+        Map<Class, Object[]> result = FixtureHelper.loadFixturesFromResource(
+                "nonExistant/", models);
+        assertTrue("Le fixture non sono vuote", result.isEmpty());
+    }
 
-	@SuppressWarnings("unchecked")
-	public void testShouldNotFailIfMissingFixtureFile() throws Exception {
-		Set<Class> models = new LinkedHashSet<Class>(CollectionUtils
-				.arrayToList(new Class[] { BigDecimal.class }));
-		Map<Class, Object[]> result = FixtureHelper.loadFixturesFromResource(
-				"fixtures/", models);
-		assertTrue("Le fixture non sono vuote", result.isEmpty());
-	}
+    @SuppressWarnings("unchecked")
+    public void testShouldNotFailIfMissingFixtureFile() throws Exception {
+        Set<Class> models = new LinkedHashSet<Class>(
+                CollectionUtils.arrayToList(new Class[]{BigDecimal.class}));
+        Map<Class, Object[]> result = FixtureHelper.loadFixturesFromResource(
+                "fixtures/", models);
+        assertTrue("Le fixture non sono vuote", result.isEmpty());
+    }
 
-	public void testShouldNotFailIfEmptyDocs() throws Exception {
-		Map<Class, Object[]> result = FixtureHelper.loadFixturesFromResource(
-				"emptyDir/", new LinkedHashSet<Class>());
-		assertTrue("Le fixture non sono vuote", result.isEmpty());
-	}
+    public void testShouldNotFailIfEmptyDocs() throws Exception {
+        Map<Class, Object[]> result = FixtureHelper.loadFixturesFromResource(
+                "emptyDir/", new LinkedHashSet<Class>());
+        assertTrue("Le fixture non sono vuote", result.isEmpty());
+    }
 
-	public void testJarLoading() throws Exception {
-		// TODO Testare il caricamento di una risorsa nei JAR
-	}
+    public void testJarLoading() throws Exception {
+        // TODO Testare il caricamento di una risorsa nei JAR
+    }
 
-	// TODO Completare il test
-	// public void testStripUTF8preamble() {
-	// byte[] stringWithPreamble = Utils
-	// .loadBinary("testdata/UTF8WithPreamble.txt");
-	// byte[] stringWithoutPreamble = Utils
-	// .loadBinary("testdata/UTF8WithoutPreamble.txt");
-	//
-	// assertTrue(FixtureHelper.hasUTF8preamble(stringWithPreamble));
-	// assertFalse(FixtureHelper.hasUTF8preamble(stringWithoutPreamble));
-	//
-	// byte[] stripped = FixtureHelper.stripUTF8preamble(stringWithPreamble);
-	// assertFalse(FixtureHelper.hasUTF8preamble(stripped));
-	// assertEquals(stringWithPreamble.length, stripped.length + 3);
-	// }
+    // TODO Completare il test
+    // public void testStripUTF8preamble() {
+    // byte[] stringWithPreamble = Utils
+    // .loadBinary("testdata/UTF8WithPreamble.txt");
+    // byte[] stringWithoutPreamble = Utils
+    // .loadBinary("testdata/UTF8WithoutPreamble.txt");
+    //
+    // assertTrue(FixtureHelper.hasUTF8preamble(stringWithPreamble));
+    // assertFalse(FixtureHelper.hasUTF8preamble(stringWithoutPreamble));
+    //
+    // byte[] stripped = FixtureHelper.stripUTF8preamble(stringWithPreamble);
+    // assertFalse(FixtureHelper.hasUTF8preamble(stripped));
+    // assertEquals(stringWithPreamble.length, stripped.length + 3);
+    // }
 
-	// TODO Completare il test
-	// public void testPrependUTF8preamble() {
-	// byte[] stringWithoutPreamble = Utils
-	// .loadBinary("testdata/UTF8WithoutPreamble.txt");
-	// byte[] preambled = FixtureHelper
-	// .prependUTF8preamble(stringWithoutPreamble);
-	// assertTrue(FixtureHelper.hasUTF8preamble(preambled));
-	// assertEquals(stringWithoutPreamble.length + 3, preambled.length);
-	// }
+    // TODO Completare il test
+    // public void testPrependUTF8preamble() {
+    // byte[] stringWithoutPreamble = Utils
+    // .loadBinary("testdata/UTF8WithoutPreamble.txt");
+    // byte[] preambled = FixtureHelper
+    // .prependUTF8preamble(stringWithoutPreamble);
+    // assertTrue(FixtureHelper.hasUTF8preamble(preambled));
+    // assertEquals(stringWithoutPreamble.length + 3, preambled.length);
+    // }
+    @SuppressWarnings("unchecked")
+    public void testBeanLoading() throws Exception {
+        Set<Class> models = new LinkedHashSet<Class>(
+                CollectionUtils.arrayToList(new Class[]{DemoBean.class}));
+        Map<Class, Object[]> objects = FixtureHelper.loadFixturesFromResource(
+                "fixtures/", models);
+        assertNotNull("Non ha ritornato la mappa di fixtures", objects);
+        assertEquals("Non carica tutti i beans", 1, objects.size());
+        assertTrue("Non crea istanze di " + DemoBean.class.getCanonicalName(),
+                objects.containsKey(DemoBean.class));
+        DemoBean[] result = (DemoBean[]) objects.get(DemoBean.class);
+        assertEquals("Non carica tutti i beans", 5, result.length);
+        for (int i = 0; i < 5; i++) {
+            assertEquals("Non ha caricato correttamente il beans numero " + i,
+                    expected[i], result[i]);
+        }
+    }
 
-	@SuppressWarnings("unchecked")
-	public void testBeanLoading() throws Exception {
-		Set<Class> models = new LinkedHashSet<Class>(CollectionUtils
-				.arrayToList(new Class[] { DemoBean.class }));
-		Map<Class, Object[]> objects = FixtureHelper.loadFixturesFromResource(
-				"fixtures/", models);
-		assertNotNull("Non ha ritornato la mappa di fixtures", objects);
-		assertEquals("Non carica tutti i beans", 1, objects.size());
-		assertTrue("Non crea istanze di " + DemoBean.class.getCanonicalName(),
-				objects.containsKey(DemoBean.class));
-		DemoBean[] result = (DemoBean[]) objects.get(DemoBean.class);
-		assertEquals("Non carica tutti i beans", 5, result.length);
-		for (int i = 0; i < 5; i++) {
-			assertEquals("Non ha caricato correttamente il beans numero " + i, expected[i], result[i]);
-		}
-	}
-
+    public void testBookLoading() {
+        Set<Class> models = new LinkedHashSet<Class>(
+                CollectionUtils.arrayToList(new Class[]{AuthorTC.class,
+                    BookTC.class
+                }));
+        Map<Class, Object[]> objects = FixtureHelper.loadFixturesFromResource(
+                "fixtures/", models);
+        assertNotNull("Non ha ritornato la mappa di fixtures", objects);
+        assertEquals(2, objects.size());
+        assertEquals(3, objects.get(BookTC.class).length);
+        assertEquals(4, objects.get(AuthorTC.class).length);
+        assertBookAuthorsAreIdentical(objects.get(BookTC.class), objects.get(AuthorTC.class));
+    }
+    
+    private void assertBookAuthorsAreIdentical(Object[] books, Object[] authors) {        
+        for (int i=0; i < books.length; i++) {
+            BookTC currentBook = (BookTC) books[i];
+            for (AuthorTC author: currentBook.getAuthors()) {
+                boolean found = false;
+                for (int j=0; j < authors.length; j++) {
+                    if (author == authors[j]) {
+                        found = true;
+                        break;
+                    }
+                }
+                assertTrue("The book author is not in the author array", found);
+            }
+        }
+    }
 }
