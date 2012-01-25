1b2d44e154e205c6404a3ea6bb38e5e4dcc00fd3
lucio.benfante <lucio.benfante@gmail.com>
2012-01-25 17:53:20 +0100

    A little generalization for the FlashHelper.

--- parancoe-web/src/main/java/org/parancoe/web/util/FlashHelper.java
+++ parancoe-web/src/main/java/org/parancoe/web/util/FlashHelper.java
@@ -24,35 +24,41 @@ import java.util.Map;
 /**
  *
  * @author Jacopo Murador <jacopo.murador at seesaw.it>
+ * @author Lucio Benfante <lucio@benfante.com>
  */
 public class FlashHelper {
+    public static String DEFAULT_NOTICE_TYPE = "notice";
+    public static String DEFAULT_ERROR_TYPE = "error";
+    public static String DEFAULT_FLASH_ATTRIBUTE = "flash";
     
     public static void setError(HttpServletRequest req, String code) {
-        Map<String, String> flash = (Map<String, String>)req.getAttribute("flash");
-        if (flash == null) flash = new HashMap<String, String>();
-        flash.put("error", code);
-        req.setAttribute("flash", flash);
+        doSet(req, code, DEFAULT_ERROR_TYPE, DEFAULT_FLASH_ATTRIBUTE);
     }
     
     public static void setNotice(HttpServletRequest req, String code) {
-        Map<String, String> flash = (Map<String, String>)req.getAttribute("flash");
-        if (flash == null) flash = new HashMap<String, String>();
-        flash.put("notice", code);
-        req.setAttribute("flash", flash);
+        doSet(req, code, DEFAULT_NOTICE_TYPE, DEFAULT_FLASH_ATTRIBUTE);
     }
     
-    public static void setRedirectError(HttpServletRequest req, String code) {
-        Map<String, String> flash = (Map<String, String>)req.getSession().getAttribute("flash");
+    public static void doSet(HttpServletRequest req, String code, String type, String attribute) {
+        Map<String, String> flash = (Map<String, String>)req.getAttribute(attribute);
         if (flash == null) flash = new HashMap<String, String>();
-        flash.put("error", code);
-        req.getSession().setAttribute("flash", flash);
+        flash.put(DEFAULT_ERROR_TYPE, code);
+        req.setAttribute(attribute, flash);
+    }
+
+    public static void setRedirectError(HttpServletRequest req, String code) {
+        doSetRedirect(req, code, DEFAULT_ERROR_TYPE, DEFAULT_FLASH_ATTRIBUTE);
     }
     
     public static void setRedirectNotice(HttpServletRequest req, String code) {
-        Map<String, String> flash = (Map<String, String>)req.getSession().getAttribute("flash");
+        doSetRedirect(req, code, DEFAULT_NOTICE_TYPE, DEFAULT_FLASH_ATTRIBUTE);
+    }
+    
+    public static void doSetRedirect(HttpServletRequest req, String code, String type, String attribute) {
+        Map<String, String> flash = (Map<String, String>)req.getSession().getAttribute(attribute);
         if (flash == null) flash = new HashMap<String, String>();
-        flash.put("notice", code);
-        req.getSession().setAttribute("flash", flash);
+        flash.put(type, code);
+        req.getSession().setAttribute(attribute, flash);
     }
     
 }
