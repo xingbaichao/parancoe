0df24c6460ab73bc32f4603fcd4b42ed2e1254f7
lucio.benfante <lucio.benfante@gmail.com>
2012-01-31 10:19:14 +0100

    Added support for fields of type Character.

--- parancoe-yaml/src/main/java/org/parancoe/yaml/ReflectionUtil.java
+++ parancoe-yaml/src/main/java/org/parancoe/yaml/ReflectionUtil.java
@@ -343,7 +343,7 @@ class ReflectionUtil {
     }
 
     public static boolean isSimpleType(Class c) {
-        return c.isPrimitive() || c == String.class || c == Integer.class || c == Long.class || c == Short.class
+        return c.isPrimitive() || c == String.class || c == Character.class || c == Integer.class || c == Long.class || c == Short.class
                 || c == Double.class || c == Float.class || c == Boolean.class || c == BigInteger.class
                 || c == BigDecimal.class || c == Date.class || c.isEnum() || c == File.class;
     }
