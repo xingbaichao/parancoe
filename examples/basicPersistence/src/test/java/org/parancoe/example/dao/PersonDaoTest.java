1023aef86d38a50e8630ad433d746277c9b53c10
Michele Franzin <michele.franzin@gmail.com>
2013-02-27 20:26:14 +0100

    EXAMPLES - license header update, basic persistence application + basic web application (evolution) gets new jUnit4 test

--- examples/basicPersistence/src/test/java/org/parancoe/example/dao/PersonDaoTest.java
+++ examples/basicPersistence/src/test/java/org/parancoe/example/dao/PersonDaoTest.java
@@ -1,37 +1,58 @@
+/**
+ * Copyright (C) 2006-2013 The Parancoe Team <info@parancoe.org>
+ *
+ * This file is part of Parancoe Example - Basic Persistence.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.parancoe.example.dao;
 
 import java.util.List;
 import javax.annotation.Resource;
 import org.parancoe.example.po.Person;
 import org.parancoe.example.test.BaseTest;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import org.junit.Test;
 
 /**
  *
  * @author lucio
  */
 public class PersonDaoTest extends BaseTest {
-    
+
     @Resource
     private PersonDao personDao;
-    
+
     /**
      * Test of findByLastName method, of class PersonDao.
      */
-    public void testFindByLastName() {
+    @Test
+    public void findByLastName() {
         List<Person> people = personDao.findByLastName("Benfante");
-        assertEquals(8, people.size());
-        for (Person p: people) {
-            assertEquals("Benfante", p.getLastName());
+        assertThat(people, hasSize(8));
+        for (Person p : people) {
+            assertThat(p.getLastName(), equalTo("Benfante"));
         }
     }
 
-    public void testFindByFirstName() {
+    @Test
+    public void findByFirstName() {
         List<Person> people = personDao.findByFirstName("Ugo");
-        assertEquals(2, people.size());
-        for (Person p: people) {
-            assertEquals("Ugo", p.getFirstName());
-            System.out.println(p);
+        assertThat(people, hasSize(2));
+        for (Person p : people) {
+            assertThat(p.getFirstName(), equalTo("Ugo"));
         }
     }
-    
 }
--- examples/basicWebAppEvolution/src/test/java/org/parancoe/basicwebappevolution/controllers/HomeControllerTest.java
+++ examples/basicWebAppEvolution/src/test/java/org/parancoe/basicwebappevolution/controllers/HomeControllerTest.java
@@ -1,34 +1,60 @@
+/**
+ * Copyright (C) 2006-2013 The Parancoe Team <info@parancoe.org>
+ *
+ * This file is part of Parancoe Example - Basic WebApp Evolution.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.parancoe.basicwebappevolution.controllers;
 
-import javax.annotation.Resource;
+import org.junit.Test;
+import org.parancoe.web.test.junit4.AbstractControllerTest;
 import org.springframework.web.servlet.ModelAndView;
-
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.parancoe.basicwebappevolution.controllers.HomeController;
-import org.parancoe.web.test.ControllerTest;
 import org.springframework.web.servlet.HandlerAdapter;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.test.web.ModelAndViewAssert.*;
 
-public class HomeControllerTest extends ControllerTest {
+/**
+ * test home controller
+ *
+ * @author michele franzin <michele at franzin.net>
+ */
+public class HomeControllerTest extends AbstractControllerTest {
 
-    @Resource
+    @Autowired
     private HomeController controller;
-    @Resource(
-    name =
-    "org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter#0")
-    private HandlerAdapter methodHandler;
+    @Autowired
+    private HandlerAdapter handler;
 
-    public void testConfiguration() {
-        assertNotNull(controller);
-        assertNotNull(methodHandler);
+    @Test
+    public void sanity() {
+        assertThat(controller, is(notNullValue()));
+        assertThat(handler, is(notNullValue()));
+        assertThat(handler.supports(controller), is(true));
     }
 
-    public void testWelcome() throws Exception {
-        resetRequestAndResponse();
-        req.setMethod("GET");
-        req.setRequestURI("/home/welcome.html");
-        req = new MockHttpServletRequest("GET", "/home/welcome.html");
-        ModelAndView mv = methodHandler.handle(req, res, controller);
-        assertEquals("welcome", mv.getViewName());
-        assertNotNull(mv.getModel().get("something"));
+    @Test
+    public void welcome() throws Exception {
+        ModelAndView mv = getPage("/home/welcome.html");
+        assertViewName(mv, "welcome");
+        assertModelAttributeAvailable(mv, "something");
+    }
+
+    private ModelAndView getPage(String pagePath) throws Exception {
+        request.setMethod("GET");
+        request.setRequestURI(pagePath);
+        return handler.handle(request, response, controller);
     }
 }
